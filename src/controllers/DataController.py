bb670e685ca2a927589ae5b9eba404d27ffe6af9
ahmedattiaaaaa <ahmedd3shush@gmail.com>
2025-06-01 10:47:17 +0300

    add files to src

--- src/controllers/DataController.py
+++ src/controllers/DataController.py
@@ -23,7 +23,7 @@ class DataController(BaseController):
 
         return True, ResponseSignal.FILE_UPLOADED_SUCCESS.value
     
-    def generate_unique_filename(self, original_filename: str, project_id: str):
+    def generate_unique_filepath(self, original_filename: str, project_id: str):
 
         random_filename = self.generate_random_string()
         project_path =ProjectController().get_project_path(project_id=project_id)
@@ -43,7 +43,8 @@ class DataController(BaseController):
                 random_filename + "__" + cleaned_file_name
             )
 
-        return new_file_path
+        return new_file_path, random_filename + "__" + cleaned_file_name
+
 
     def get_clean_file_name (self, original_filename: str):
 
